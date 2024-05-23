2b756a190a550782f4a821056ba961fc1b649eb0
Jayesh Hyalij <115018197+Jayesh-Hyalij@users.noreply.github.com>
2024-05-23 09:27:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,55 @@ import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" }
     };
+    this.renderChoice = this.renderBallOrButton.bind(this)
+    this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  };
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+  buttonClickHandler() {
+    this.setState({
+      renderBall: true
+    });
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
+  handleKeyDown(event) {
+    if (event.key === 'ArrowRight' || event.keyCode === 39) {
+      this.setState(prevState => ({
+        posi: prevState.posi + 5
+      }), () => {
+        this.setState({
+          ballPosition: { left: `${this.state.posi}px` }
+        });
+      });
     }
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>
+    } else {
+      return <button onClick={this.buttonClickHandler} >Start</button>
     }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    return (
+      <div className="playground">
+        {this.renderBallOrButton()}
+      </div>
+    )
+  }
 }
 
 
